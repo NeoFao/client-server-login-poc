04ec8460b99c5b87250ead8f1da9ec6c726c54c1
NeoFao <fabrizioespinozaarce@gmail.com>
2026-03-04 17:57:21 -0600

    Add comments to MeResponse record
    Added comments in Spanish to describe the purpose of the MeResponse record.

--- client-server-login-poc/ClientServerLoginPoC/ClientServerLoginPoC/Models/MeResponse.cs
+++ client-server-login-poc/ClientServerLoginPoC/ClientServerLoginPoC/Models/MeResponse.cs
@@ -1,5 +1,9 @@
 ﻿namespace ClientServerLoginPoC.Models
 {
+    // se usa típicamente como respuesta (response body) en JSON
+    // para indicar "quién soy" según el token enviado.
+    // Genera automáticamente una propiedad pública init-only (inmutable tras crearse)
     public record MeResponse(string Email);
 
 }
+
