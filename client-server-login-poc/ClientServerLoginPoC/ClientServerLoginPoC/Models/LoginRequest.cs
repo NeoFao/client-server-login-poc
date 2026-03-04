0cfdf21fc87690ee3640f07c388acd4d08c34802
NeoFao <fabrizioespinozaarce@gmail.com>
2026-03-04 17:53:12 -0600

    Add comment to LoginRequest for clarity
    Added a comment to clarify the purpose of the LoginRequest record.

--- client-server-login-poc/ClientServerLoginPoC/ClientServerLoginPoC/Models/LoginRequest.cs
+++ client-server-login-poc/ClientServerLoginPoC/ClientServerLoginPoC/Models/LoginRequest.cs
@@ -1,5 +1,7 @@
 ﻿namespace ClientServerLoginPoC.Models
 {
+    // Este usa como el body de un POST (JSON), Controllers/AuthController
     public record LoginRequest(string Email, string Password);
 
 }
+
